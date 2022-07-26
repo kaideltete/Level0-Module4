80c2fc8c9a17d661a42180c356a48f3afd81a259
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-26 14:04:34 -0700

    Update _b_sleepy_head.py

--- _02_boolean /_b_sleepy_head.py
+++ _02_boolean /_b_sleepy_head.py
@@ -2,9 +2,12 @@
 Use boolean variables to control program logic between two different code
 paths.
 """
+import turtle
 
 
 if __name__ == '__main__':
+    my_turtle = turtle.Turtle()
+
     # TODO)
     #  1. Use a boolean variable to indicate if it is the weekend.
     #     Display a different message to the user depending on whether it is
@@ -17,4 +20,31 @@ if __name__ == '__main__':
     #  4. Use two boolean variables, one to indicate if a shape should be red,
     #     the other to indicate if the shape is to be square. When both
     #     variables are true, use a turtle to draw a red square.
-    pass
+
+    wekend=False
+    if wekend == False:
+        print('it is not the wekend')
+
+    test=True
+    if test == True:
+        print('the studint passed the test')
+
+    game=False
+    if game == False:
+        print('game over')
+
+    shape_col_red=True
+    if shape_col_red == True:
+        window = turtle.Screen()
+        window.bgcolor('white')
+        my_turtle.shape("turtle")
+        my_turtle.speed(2)
+        my_turtle.color('green')
+        my_turtle.pencolor('red')
+        my_turtle.begin_fill()
+        for i in range(4):
+            my_turtle.forward(100)
+            my_turtle.left(90)
+        my_turtle.end_fill()
+
+pass
